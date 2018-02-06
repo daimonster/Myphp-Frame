1e9700834f48433c6b40d039e678c7809ad8515b
daimonster <546508148@qq.com>
2018-02-07 05:01:44 +0800

    凌晨学习了一点mvc

--- fastphp/Fast.php
+++ fastphp/Fast.php
@@ -17,7 +17,35 @@ class Fastphp
     //运行程序
     public function run()
     {
-        spl_autoload_register()
+        spl_autoload_register(array(this,'loadClass'));
+        $this->setReporting();
+        $this->removeMagicQuotes();
+        $this->unregisterGlobals();
+        $this->route();
+    }
+
+    //路由处理
+    public function route()
+    {
+        $controllerName = $this->config['defaultController'];
+        $actionName = $this->config['defaultAction'];
+        $param = array();
+
+        $url = $_SERVER['REQUEST_URL'];
+        //清除?之后的内容
+        $position = strpos($url,'?');
+        $url = $position === false ? $url : substr($url,0,$position);
+
+        //删除前后的'\'
+        $url = trim($url,'/');
+
+        if($url)
+        {
+            //使用'/'分割字符串,并保存在数组中
+            $urlArray = explode('/',$url);
+            //删除空的数组元素
+            $urlArray = array_filter($urlArray);
+        }
     }
 }
 
