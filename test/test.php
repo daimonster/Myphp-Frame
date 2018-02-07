eb075678382d752cc4e7e73e5eedaa846d8175cb
daimonster <546508148@qq.com>
2018-02-08 01:37:27 +0800

    删除test文件内容

--- test/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$arr = array(1,1,3,4,'9' => 10, '12' => 100);
-print_r($arr);
-// define("CONSTANT","Hello world");
-// echo CONSTANT; // 输出 "Hello world."
-// echo Constant;
-$data = "foo:*:1023:1000::/home/foo:/bin/sh";
-$string = explode(":", $data);
-print_r($string); 
