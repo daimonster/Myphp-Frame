3938e67c69de2396326161237a33fafc5ac53a81
daimonster <546508148@qq.com>
2018-02-11 18:30:20 +0800

    建立了数据库数据,开始部署框架

--- base/View.php
+++ base/View.php
@@ -23,6 +23,39 @@ class View
     // 渲染显示
     public function render()
     {
+        extract($this->variables);
+        $defaultHeader = APP_PATH . 'app/views/header.php';
+        $defaultFooter = APP_PATH . 'app/views/footer.php';
+        $controllerHeader = APP_PATH . 'app/views/' . $this->_controller . "/header.php";
+        $controllerFooter = APP_PATH . "app/views/" . $this->_controller . "/footer.php";
+        $controllerLayout = APP_PATH . "app/views/" . $this->_controller. '/' . $this->action . 'php';
         
+        //页头文件
+        if(is_file($controllerLayout))
+        {
+            include ($controllerHeader);
+        }
+        else
+        {
+            include ($defaultHeader);
+        }
+        //判断视图文件是否存在
+        if(is_file($controllerLayout))
+        {
+            include ($controllerLayout);
+        }
+        else
+        {
+            echo "<h1>无法找到视图文件</h1>";
+        }
+        //页脚文件
+        if(is_file($controllerFooter))
+        {
+            include ($controllerFooter);
+        }
+        else
+        {
+            include ($defaultFooter);
+        }
     }
 }
